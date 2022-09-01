b58c74ca96ff70335e388b9770b79b45e2b5961f
Rahul <rgilhotra.tech@gmail.com>
2022-09-01 11:44:32 +0530

    Only compressor part optimized.

---
+++ Huffman.java
@@ -0,0 +1,54 @@
+package MISC;
+
+import java.util.*;
+
+public class Huffman
+{
+    private Node root;
+    public String encode(String text)
+    {
+        char[] ch = text.toCharArray();
+
+        PriorityQueue<Node> q = countFrequencies(ch);
+
+        root = createTree(q);
+
+        Map<Character, String> codemap = createCodeMap();
+        StringBuilder data = new StringBuilder();
+
+        for (char c : ch) data.append(codemap.get(c));
+
+        return data.toString();
+    }
+
+    PriorityQueue<Node> countFrequencies(char[] letters)
+    {
+        Map<Character, Node> count = new HashMap<>();
+        for (char ch : letters)
+        {
+            if (!count.containsKey(ch)) count.put(ch, new Node(ch));
+            count.get(ch).add();
+        }
+        return new PriorityQueue<>(count.values());
+    }
+
+    Node createTree(PriorityQueue<Node> nodes)
+    {
+        while (true)
+        {
+            Node node1 = nodes.poll();
+            Node node2 = nodes.poll();
+
+            Node parent = new Node(node1, node2);
+
+            if (nodes.isEmpty()) return parent;
+
+            nodes.add(parent);
+        }
+    }
+    Map<Character, String> createCodeMap() {
+        Map<Character, String> result = new TreeMap<>();
+        root.fillCodeMap(result, "");
+        return result;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+package MISC;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        while(true)
+        {
+            showIntro();
+            System.out.println("\nenter the text or \"exit\" to exit from program: ");
+
+            String text = input.nextLine().trim();
+
+            if(text.toLowerCase().equals("exit"))
+            {
+                System.out.println("_-_-_-Thank you for using this program\nHave a nice day-_-_-_");
+                return;
+            }
+
+            Huffman obj = new Huffman();
+            String data  = obj.encode(text);
+            System.out.println(data);
+        }
+    }
+    public static void showIntro()
+    {
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+        System.out.println("*-*-*-*- HUFFMAN BACK N FORTH  by Rahul Gilhotra -*-*-*-*");
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    }
+}
